65c2a20f10c16a8f079775896738dd9bde47a0c9
Jiameng <jmfan@bu.edu>
2020-02-05 19:16:08 -0500

    get scene graph

---
+++ data/getFullImageData.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+wget https://cs.stanford.edu/people/rak248/VG_100K_2/images.zip
+wget https://cs.stanford.edu/people/rak248/VG_100K_2/images2.zip
+unzip images.zip
+unzip images2.zip
+rm images.zip
+rm images2.zip
